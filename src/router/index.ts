22e254deca66cd37684985ba7e511085cfd79d65
ChanchakornJ <chanchakornj@gmail.com>
2026-08-02 22:57:13 +0700

    6.1 init

---
+++ src/router/index.ts
@@ -0,0 +1,77 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventListView from '@/views/EventListView.vue'
+import StudentListView from '@/views/StudentListView.vue'
+import AboutView from '@/views/AboutView.vue'
+import EventDetailView from '@/views/event/DetailView.vue'
+import EventRegisterView from '@/views/event/RegisterView.vue'
+import EventEditView from '@/views/event/EditView.vue'
+import EventLayoutView from '@/views/event/LayoutView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
+import NetworkErrorView from '@/views/event/NetworkErrorView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'event-list-view',
+      component: EventListView,
+      props: (route) => ({
+        page: parseInt(route.query.page?.toString() || '1'),
+        perPage: Number(route.query.limit) || 2
+      })
+      
+    },
+    {
+      path: '/event/:id',
+      name: 'event-layout-view',
+      component: EventLayoutView,
+      props: true,
+      children: [
+        {
+          path: '',
+          name: 'event-detail-view',
+          component: EventDetailView
+        },
+        {
+          path: 'register',
+          name: 'event-register-view',
+          component: EventRegisterView
+        },
+        {
+          path: 'edit',
+          name: 'event-edit-view',
+          component: EventEditView
+        }
+      ]
+    },
+    {
+      path: '/about',
+      name: 'about',
+     component: AboutView
+    },
+    {
+      path: '/404/:resource',
+      name: '404-resource-view',
+      component: NotFoundView,
+      props:true
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView
+    }, 
+    {
+      path: '/network-error',
+      name: '/network-error-view',
+      component: NetworkErrorView
+    },
+    {
+      path: '/student',
+      name: 'student-list-view',
+     component: StudentListView
+    }
+  ],
+})
+
+export default router
---
+++ src/views/StudentListView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import StudentCard from '@/components/StudentCard.vue'
+import type { Student } from '@/types'
+import {ref, onMounted} from 'vue'
+import StudentService from '@/services/StudentService'
+const students = ref<Student[] | null>(null)
+
+onMounted(() =>{
+   StudentService.getEvents()
+    .then((response) =>{
+      console.log(response.data)
+      students.value = response.data
+    })
+    .catch((error) => {
+      console.error('There was an error!', error)
+    })
+})
+</script>
+<template>
+    <h1>List of Students</h1>
+
+  <div class="events">
+    <StudentCard v-for="student in students" :key="student.id" :student="student"/>
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
